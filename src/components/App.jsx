7bb3f64f78f03fafdb8336957b025a954d8163d7
Jennifer <jsmit032@ucr.edu>
2020-04-14 15:21:33 -0500

    use map function to render notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,24 @@ import React from 'react';
 import Heading from './Heading';
 import Footer from './Footer';
 import Note from './Note';
+import Entry from '../notes';
+
+//console.log(Entry);
 
 function App() {
     return (
         <div>
             <Heading />
-            <Note />
+            {Entry.map(noteEntry => (
+                <Note 
+                    key={noteEntry.key}
+                    title={noteEntry.title}
+                    content={noteEntry.content}
+                />
+            ))}
             <Footer />
         </div>
-    )
+    );
 }
 
 export default App;
