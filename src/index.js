269c28f1075d52ff2a49bb096f919ea74e9379c8
Jennifer <jsmit032@ucr.edu>
2020-04-14 15:05:58 -0500

    start of Challenge 2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-//5. Create a Note.jsx component to show a <div> element with a
-//<h1> for a title and a <p> for the content.
-//6. Make sure that the final website is styled like the example shown here:
-//https://l1pp6.csb.app/
-
-//HINT: You will need to study the classes in teh styles.css file to appy styling.
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
@@ -11,3 +5,6 @@ import App from './components/App';
 ReactDOM.render( 
     <App />
     , document.getElementById('root'));
+
+//Challenge. Render all the notes inside notes.js as a seperate Note
+//component.
