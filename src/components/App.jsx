ce58f7e88024590467372e342e1ae9d1d7f987a6
Jennifer <jsmit032@ucr.edu>
2020-04-18 14:07:03 -0500

    adjusted import Heading and styles to start Part 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "./Header";
+import Header from "./Heading";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
