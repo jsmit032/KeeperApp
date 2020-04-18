dfeccfd7c5ebfe88964c2bb0663e33723180b93c
Jennifer <jsmit032@ucr.edu>
2020-04-18 15:38:28 -0500

    tracking changed states of input fields, doesn't change the states of array of notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Heading";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+    
+    const [notes, setNotes] = useState({
+        title: "",
+        content: ""
+      });
+
+    function addNote(inputObject, event) {
+        console.log(inputObject);
+        setNotes(prevNote => {
+            return {...prevNote, inputObject};
+        });
+        event.preventDefault();
+    }
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea onAdd={addNote} />
+      {/* {notes.map((note, index)=> (
+        <Note 
+            key={index}
+            id={index}
+            title={note.title}
+            content={note.content}
+        />
+      ))} */}
       <Footer />
     </div>
   );
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+
+    const [inputObject, setInputObject] = useState({
+        title: "",
+        content: ""
+    });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setInputObject(prevValue => {
+      return {
+        ...prevValue,
+        [name]: value
+      };
+    });
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input onChange={handleChange} name="title" placeholder="Title" value={inputObject.title} />
+        <textarea onChange={handleChange} name="content" placeholder="Take a note..." rows="3" value={inputObject.content} />
+        <button onClick={() => {
+            props.onAdd(inputObject);
+            setInputObject("");
+        }}>Add</button>
       </form>
     </div>
   );
