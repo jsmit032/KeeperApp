793920b3c6c76bcfc6c8b7c5d9a0361de26c9eb4
Jennifer <jsmit032@ucr.edu>
2020-04-19 18:12:10 -0500

    add clicked expansion to content textarea

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 import AddIcon from '@material-ui/icons/Add';
 import Fab from '@material-ui/core/Fab';
+import Zoom from '@material-ui/core/Zoom';
 
 function CreateArea(props) {
   const [note, setNote] = useState({
     title: "",
     content: ""
   });
+
+const [clickedInput, setClickedInput] = useState(false);
+ 
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -19,32 +23,47 @@ function CreateArea(props) {
     });
   }
 
+  function handleClick() {
+      setClickedInput(true);
+  }
+
   function submitNote(event) {
     props.onAdd(note);
     setNote({
       title: "",
       content: ""
     });
+    setClickedInput(false);
     event.preventDefault();
   }
 
   return (
     <div>
       <form className="create-note">
-        <input
+      {clickedInput ? <input
           name="title"
           onChange={handleChange}
           value={note.title}
           placeholder="Title"
-        />
-        <textarea
+        /> : null}
+        {clickedInput ? <textarea
           name="content"
           onChange={handleChange}
+          onClick={handleClick}
           value={note.content}
           placeholder="Take a note..."
-          rows="3"
-        />
-        <Fab onClick={submitNote}><AddIcon /></Fab>
+          rows="3" 
+        />: <textarea
+          name="content"
+          onChange={handleChange}
+          onClick={handleClick}
+          value={note.content}
+          placeholder="Take a note..."
+          rows="1" 
+        />}
+        {clickedInput ? <Zoom in={true}>
+            <Fab onClick={submitNote}><AddIcon /></Fab>
+        </Zoom> : null}
       </form>
     </div>
   );
