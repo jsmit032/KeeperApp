2d9c2087d95a4b1427f5072521c6ede1d25d7f75
Jennifer <jsmit032@ucr.edu>
2020-04-19 16:31:09 -0500

    add style icons from pre-made components

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
 
 function CreateArea(props) {
   const [note, setNote] = useState({
@@ -42,7 +44,7 @@ function CreateArea(props) {
           placeholder="Take a note..."
           rows="3"
         />
-        <button onClick={submitNote}>Add</button>
+        <Fab onClick={submitNote}><AddIcon /></Fab>
       </form>
     </div>
   );
