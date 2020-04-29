38ebdd6f027e62f8b0e6a20f5a0310ec4444c57f
Jennifer <jsmit032@ucr.edu>
2020-04-29 15:18:14 -0500

    add build folder to client

---
+++ client/build/precache-manifest.b05c467b4f8356ebd90ff8f58fe0ba3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "780567318202977561b0136820a5dc34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6625a7889778a8ca7191",
+    "url": "/static/css/main.c5053897.chunk.css"
+  },
+  {
+    "revision": "63139550458b9e34a5f8",
+    "url": "/static/js/2.0e24cdf1.chunk.js"
+  },
+  {
+    "revision": "6625a7889778a8ca7191",
+    "url": "/static/js/main.0827060f.chunk.js"
+  },
+  {
+    "revision": "25290cbd15fdc0a498c6",
+    "url": "/static/js/runtime-main.add7c3e5.js"
+  }
+]);
